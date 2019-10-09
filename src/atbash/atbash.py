957b33cedb8c56b91cace5a14272ade1db4ac2f0
Sean Mitchell <sm0077@uah.edu>
2019-10-09 15:36:21 -0500

    Testing algorithm for atbash.

--- src/atbash/atbash.py
+++ src/atbash/atbash.py
@@ -1,21 +1,25 @@
 from file_utils import *
 
-def Encrypt(plaintext, shift):
+def Encrypt(plaintext):
 
     temp_string = ""
     for c in plaintext:
-        if c.lower == "z":
-            c = "a"
-        temp = (ord(c) + 13)
-        if temp < ord('a'):
-            temp_string += " "
-        else:
-            c = chr(temp)
-            temp_string += c
+        distance = ord('a') - ord(c)
+        print(distance)
+        swap = ord('z') + distance
+        print(chr(swap))
+        # if c.lower == "z":
+        #     c = "a"
+        # temp = (ord(c) + 13)
+        # if temp < ord('a'):
+        #     temp_string += " "
+        # else:
+        #     c = chr(temp)
+        #     temp_string += c
 
     return temp_string
 
-def Decrypt(ciphertext, shift):
+def Decrypt(ciphertext):
 
     temp_string = ""
     for c in ciphertext:
--- src/atbash/atbash_driver.py
+++ src/atbash/atbash_driver.py
@@ -1,8 +1,8 @@
 from atbash import *
 
-word = "defend the east wall of the castle"
+word = "abcdefg"
 print("Original word: " + word)
-encrpyted_word = Encrypt(word, 3)
-print("Encrypted word: " + encrpyted_word)
-decrpyted_word = Decrypt(encrpyted_word,3)
-print("Decrypted word: " + decrpyted_word)
+encrpyted_word = Encrypt(word)
+#print("Encrypted word: " + encrpyted_word)
+#decrpyted_word = Decrypt(encrpyted_word,3)
+#print("Decrypted word: " + decrpyted_word)
