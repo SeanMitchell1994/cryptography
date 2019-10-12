58d70d6a79e3f55def103caf306ef58cc3d49a4b
Sean <sm0077@uah.edu>
2019-10-12 11:45:53 -0500

    Added rail_fence.py.

---
+++ src/rail-fence/rail_fence.py
@@ -0,0 +1,12 @@
+from file_utils import *
+from ascii_utils import *
+
+def Encrypt(plaintext, shift):
+
+    temp_string = ""
+    return temp_string
+
+def Decrypt(ciphertext, shift):
+
+    temp_string = ""
+    return temp_string
