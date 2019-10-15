eeb31abf74f4ea4909667872c61a639cd45e1b86
Sean Mitchell <sm0077@uah.edu>
2019-10-15 18:37:38 -0500

    Starting to set up rail-fence algorithm.

--- src/rail-fence/rail_fence.py
+++ src/rail-fence/rail_fence.py
@@ -1,12 +1,18 @@
 from file_utils import *
 from ascii_utils import *
 
-def Encrypt(plaintext, key):
+def Encrypt(plaintext, rails):
 
     temp_string = ""
+    length = len(plaintext)
+    for i in range(rails):
+        for j in range(length):
+            print("* ",end="")
+        print("")
+
     return temp_string
 
-def Decrypt(ciphertext, key):
+def Decrypt(ciphertext, rails):
 
     temp_string = ""
     return temp_string
