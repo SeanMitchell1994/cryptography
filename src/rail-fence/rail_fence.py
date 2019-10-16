a7ebde368c151b005a73615abb034bcf3a53495f
Sean Mitchell <sm0077@uah.edu>
2019-10-15 19:02:21 -0500

    Improvements to rail-fence algorithm.

--- src/rail-fence/rail_fence.py
+++ src/rail-fence/rail_fence.py
@@ -5,10 +5,30 @@ def Encrypt(plaintext, rails):
 
     temp_string = ""
     length = len(plaintext)
-    for i in range(rails):
-        for j in range(length):
-            print("* ",end="")
-        print("")
+    string_arr = [""] * 3
+    count = 0
+
+    #for i in range(rails):
+    #    for j in range(length):
+    #        print("* ",end="")
+    #    print("")
+
+    for c in range(6):
+        for rail in range(rails):
+            if rail == count:
+                #print("1 ")
+                string_arr[rail] += str(count)
+            #else:
+                #print("*")
+        #print("")
+
+        count += 1
+        if (count >= 3):
+            count = 0
+
+    for i in string_arr:
+        temp_string += i
+    #print(string_arr)
 
     return temp_string
 
--- src/rail-fence/rail_fence_driver.py
+++ src/rail-fence/rail_fence_driver.py
@@ -3,6 +3,6 @@ from rail_fence import *
 word = "defend the east wall of the castle"
 print("Original word: " + word)
 encrpyted_word = Encrypt(word, 3)
-#print("Encrypted word: " + encrpyted_word)
+print("Encrypted word: " + encrpyted_word)
 #decrpyted_word = Decrypt(encrpyted_word,3)
 #print("Decrypted word: " + decrpyted_word)
