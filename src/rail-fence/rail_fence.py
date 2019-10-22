23ef1fe55fc7c288f92518671c42c99326c98134
Sean Mitchell <sm0077@uah.edu>
2019-10-22 18:40:05 -0500

    Maybe working decode for rails = 2.

--- src/rail-fence/rail_fence.py
+++ src/rail-fence/rail_fence.py
@@ -46,15 +46,25 @@ def Decrypt(ciphertext, rails):
     #i = 0
     #j = 0
 
-    for i in range(0,len(ciphertext),rails):
-        print(ciphertext[(i - rails):i])
+    #for i in range(0,len(ciphertext),rails):
+    #    print(ciphertext[(i - rails):i])
 
+    # Decode algorithm is:
+    # assume c is starting character
+    # move forward key + 1 characters
+    # next character is next decoded character
+    # go back to c and increment
+    #
+    # Seems to work only for key of 2
+    for i in range(len(ciphertext) // rails):       # Floor division by # of rails
+        temp_string += ciphertext[i]
+        if ((i + rails + 1) < len(ciphertext)):
+            #print(ciphertext[i + rails + 1])
+            temp_string += ciphertext[i + rails + 1]
+        else:
+            break
 
     return temp_string
-
-def test2(n):
-    for i in list(range(0, n + 1)) + list(range(n-1, -1, -1)):
-        print(i)
 
 def Next(count):
     # Returns next index of the rail
--- src/rail-fence/rail_fence_driver.py
+++ src/rail-fence/rail_fence_driver.py
@@ -1,8 +1,8 @@
 from rail_fence import *
 
-word = "defend the east wall of the castle"
+word = "defend"
 print("Original word: " + word)
-encrpyted_word = Encrypt(word, 4)
+encrpyted_word = Encrypt(word, 2)
 print("Encrypted word: " + encrpyted_word)
-decrpyted_word = Decrypt(encrpyted_word,4)
-#print("Decrypted word: " + decrpyted_word)
+decrpyted_word = Decrypt(encrpyted_word,2)
+print("Decrypted word: " + decrpyted_word)
